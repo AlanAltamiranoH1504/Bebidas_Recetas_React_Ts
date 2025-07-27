54bb2b75bee2c51499bcb134301a4d75ddb5c9f9
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-07-27 11:42:40 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <>
+            <h2 className="text-4xl text-center font-fjalla font-orbitron">Hola Mundo</h2>
+        </>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                fjalla: ['"Fjalla One"', 'sans-serif'],
+                orbitron: ["Orbitron", "sans-serif"],
+            }
+        },
+    },
+    plugins: [],
+}
+
